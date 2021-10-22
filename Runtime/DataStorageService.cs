166e472f2e0bdce59fc041864488d86d90d0e253
TerentevVlad <ter.vlad.ter@gmail.com>
2021-10-22 18:46:01 +0300

    Init StorageModule 1.0.0

---
+++ Runtime/DataStorageService.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections.Generic;
+using LittleBit.Modules.CoreModule;
+using Zenject;
+
+namespace LittleBit.Modules.StorageModule
+{
+    public class DataStorageService : IDataStorageService
+    {
+        private readonly Dictionary<string, Data> _storage;
+        private readonly Dictionary<string, List<Action>> _listeners;
+        private readonly ISaveService _saveService;
+
+        private InfoDataStorageService _infoDataStorageService;
+
+        [Inject]
+        public DataStorageService(ISaveService saveService, InfoDataStorageService infoDataStorageService)
+        {
+            _storage = new Dictionary<string, Data>();
+            _listeners = new Dictionary<string, List<Action>>();
+            _saveService = saveService;
+            _infoDataStorageService = infoDataStorageService;
+            _infoDataStorageService.Clear();
+        }
+
+        public T GetData<T>(string key) where T : Data, new()
+        {
+            if (!_storage.ContainsKey(key))
+            {
+                T data = _saveService.LoadData<T>(key);
+                if (data == null)
+                {
+                    data = new T();
+                }
+
+                _storage.Add(key, data);
+            }
+
+            _infoDataStorageService.UpdateData(key, _storage[key]);
+            return (T) _storage[key];
+        }
+
+        public void SetData<T>(string key, T data) where T : Data
+        {
+            if (!_storage.ContainsKey(key))
+            {
+                _storage.Add(key, data);
+            }
+            else
+            {
+                _storage[key] = data;
+            }
+
+            if (_listeners.ContainsKey(key) && _storage.ContainsKey(key))
+            {
+                var listeners = _listeners[key];
+                foreach (var listener in listeners)
+                {
+                    listener.Invoke();
+                }
+            }
+
+            _saveService.SaveData(key, data);
+            _infoDataStorageService.UpdateData(key, data);
+        }
+
+        public void AddUpdateDataListener(string key, Action onUpdateData)
+        {
+            if (!_listeners.ContainsKey(key))
+            {
+                _listeners.Add(key, new List<Action>());
+            }
+
+            _listeners[key].Add(onUpdateData);
+        }
+
+        public void RemoveUpdateDataListener(string key, Action onUpdateData)
+        {
+            if (_listeners.ContainsKey(key))
+            {
+                _listeners[key].Remove(onUpdateData);
+            }
+        }
+    }
+}
